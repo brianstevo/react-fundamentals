41797e6571675d5c9b1fef86e9c5a86ebbc753af
brianstevo <brianaranha76@gmail.com>
2023-01-29 16:23:14 +0530

    navbar made responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 import { pageLinks, socialLinks } from "../data"
 const Navbar = () => {
+  const [isActive, setActive] = useState("false")
+
+  const handleToggle = () => {
+    setActive(!isActive)
+  }
+
   return (
     <>
       <nav className="navbar">
         <div className="nav-center">
           <div className="nav-header">
             <img src="./images/logo.svg" className="nav-logo" alt="backroads" />
-            <button type="button" className="nav-toggle" id="nav-toggle">
+            <button type="button" className="nav-toggle" id="nav-toggle" onClick={handleToggle}>
               <i className="fas fa-bars"></i>
             </button>
           </div>
-          <ul className="nav-links" id="nav-links">
+          <ul className={isActive ? "nav-links hide-links" : "nav-links "} id="nav-links">
             {pageLinks.map((element) => {
               return (
                 <li key={element.id}>
-                  <a href={element.href} className="nav-link" rel="noopener">
+                  <a href={element.href} className="nav-link" rel="noopener" onClick={handleToggle}>
                     {element.text}
                   </a>
                 </li>
